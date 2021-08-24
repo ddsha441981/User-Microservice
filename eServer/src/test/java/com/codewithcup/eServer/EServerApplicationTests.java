1eb2413a035f0161b2604aa70cfb8ceed591b21c
ddsha441981 <ddsa44191@gmail.com>
2021-08-24 11:14:22 +0530

    eServer Config

---
+++ eServer/src/test/java/com/codewithcup/eServer/EServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithcup.eServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
